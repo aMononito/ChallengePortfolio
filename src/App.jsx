b435bb68a1d2b1d85b051aa136ce466766f46792
aMononito <pedro@torstig.us>
2025-02-05 15:41:00 -0500

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,40 @@
+import Projects from "./components/Projects.jsx";
+
+const  = [
+  {
+    id: 1,
+    name: "Milk",
+    purchased: true
+  },
+  {
+    id: 2,
+    name: "Eggs",
+    purchased: true
+  },
+  {
+    id: 3,
+    name: "Cheese",
+    purchased: false
+  },
+  {
+    id: 4,
+    name: "Cake Mix",
+    purchased: false
+  },
+  {
+    id: 5,
+    name: "Carrots",
+    purchased: false
+  },
+  {
+    id: 6,
+    name: "Juice",
+    purchased: true
+  }
+];
+
+function App() {
+  return <List groceries={groceries} />;
+}
+
+export default App;
